badbed8d11dbaaa234be88818ca55a327d53dc1c
PabloPirri <pablopirri.83@gmail.com>
2023-04-17 10:00:24 -0300

    reentrega

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// import { promises as fs } from 'fs'
+import { error } from 'console';
+import { promises as fs } from 'fs'
 
 class Product {
     constructor(title, description, price, thumbnail, code, stock) {
@@ -20,133 +21,176 @@ class ProductManager {
 
     async addProduct(title, description, price, thumbnail, code, stock) {
 
-      // Validar que todos los campos sean obligatorios
-      if (!title || !description || !price || !thumbnail || !code || !stock) {
-        console.error('Error: todos los campos son obligatorios');
-        return;
+      try{
+        // Creo una constante para no repetir codigo
+        const prods = await productManager.getProducts();
+  
+        // Validar que todos los campos sean obligatorios
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+          throw new Error('Error: todos los campos son obligatorios');
+        }
+
+        // Validar que no se repita el campo "code"
+        if (prods.some(product => product.code === code)) {
+          throw new Error('Error: ya existe un producto con este código');
+        }
+
+        // Crear nuevo producto
+        const newProduct = new Product(title, description, price, thumbnail, code, stock);   
+
+        // Generar id autoincrementable
+        newProduct.id = prods.length + 1;
+        
+        // Agregar nuevo producto al arreglo de productos
+        prods.push(newProduct);
+  
+        //Reescribe el archivo consultado con el nuevo producto convertido a String 
+        await fs.writeFile(this.path, JSON.stringify(prods))
+  
+        return console.log(`Producto agregado con éxito: ${newProduct.title}`);
+
+      }catch (error) {
+        console.error(`Error al agregar producto: ${error.message}`);
       }
-  
-      // Validar que no se repita el campo "code"
-      if (this.products.some(product => product.code === code)) {
-        console.error('Error: ya existe un producto con este código');
-        return;
-      }
-  
-      // Crear nuevo producto
-      const newProduct = new Product(title, description, price, thumbnail, code, stock);
-  
-      // Generar id autoincrementable
-      newProduct.id = this.products.length + 1;
-  
-      // Agregar nuevo producto al arreglo de productos
-      this.products.push(newProduct);
-      console.log(`Producto agregado con éxito: ${newProduct.title}`);
     }
   
     async getProducts() {
-      return this.products;
+
+      // Leo el archivo txt y lo retorno como JSON
+      const prods = await fs.readFile(this.path, 'utf-8')
+      return JSON.parse(prods)
     }
   
     async getProductById(id) {
-      const product = this.products.find(product => product.id === id);
-      if (!product) {
-        console.error(`Error: no se encontró ningún producto con id ${id}`);
-        return;
+
+      try{
+        // Creo una constante para no repetir codigo
+        const prods = await productManager.getProducts();
+  
+        // Encontrar el producto por su ID
+        const product = prods.some(prod => prod.id === parseInt(id));
+        if (!product) {
+          throw new Error(`Error: no se encontró ningún producto con id ${id}`);
+        }
+        return console.log(prods.find(prod => prod.id === parseInt(id)));
+      }catch(error){
+        console.error(error)
+       }
+
       }
-      return product;
-    }
 
     async updateProduct(id, title, description, price, thumbnail, code, stock) {
-      // Encontrar el producto por su ID
-      const productIndex = this.products.findIndex(product => product.id === id);
-  
-      if (productIndex === -1) {
-        console.error(`Error: no se encontró ningún producto con id ${id}`);
-        return;
-      }
-  
-      // Validar que todos los campos sean obligatorios
-      if (!title || !description || !price || !thumbnail || !code || !stock) {
-        console.error('Error: todos los campos son obligatorios');
-        return;
-      }
-  
-      // Validar que no se repita el campo "code" excepto para el producto que se está actualizando
-      if (this.products.some(product => product.code === code && product.id !== id)) {
-        console.error('Error: ya existe un producto con este código');
-        return;
-      }
+      try{
 
-      // Actualizar los campos del producto
-      this.products[productIndex].title = title;
-      this.products[productIndex].description = description;
-      this.products[productIndex].price = price;
-      this.products[productIndex].thumbnail = thumbnail;
-      this.products[productIndex].code = code;
-      this.products[productIndex].stock = stock;
+        // Creo una constante para no repetir codigo
+        const prods = await productManager.getProducts();
   
-      console.log(`Producto actualizado con éxito: ${title}`);
+        // Encontrar el producto por su ID
+        const productIndex = prods.findIndex(product => product.id === id);
+    
+        if (productIndex === -1) {
+          throw new Error(`Error: no se encontró ningún producto con id ${id}`);
+        }
+    
+        // Validar que todos los campos sean obligatorios
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+          throw new Error('Error: todos los campos son obligatorios');
+        }
+    
+        // Validar que no se repita el campo "code" excepto para el producto que se está actualizando
+        if (prods.some(product => product.code === code && product.id !== id)) {
+          throw new Error('Error: ya existe un producto con este código');
+        }
+        // Actualizar los campos del producto
+        prods[productIndex].title = title;
+        prods[productIndex].description = description;
+        prods[productIndex].price = price;
+        prods[productIndex].thumbnail = thumbnail;
+        prods[productIndex].code = code;
+        prods[productIndex].stock = stock;
+
+        //Reescribe el archivo consultado con el producto actualizado convertido a String 
+        await fs.writeFile(this.path, JSON.stringify(prods))
+        return console.log(`Producto actualizado con éxito: ${title}`);
+      }catch(error){
+        console.error(error);
+        throw error;
+       }
     }
   
     async deleteProduct(id) {
-      // Encontrar el producto por su ID
-      const productIndex = this.products.findIndex(product => product.id === id);
+      try{
+        // Creo una constante para no repetir codigo
+        const prods = await productManager.getProducts();
   
-      if (productIndex === -1) {
-        console.error(`Error: no se encontró ningún producto con id ${id}`);
-        return;
-      }
-  
-      // Eliminar el producto del arreglo de productos
-      const deletedProduct = this.products.splice(productIndex, 1)[0];
-  
-      console.log(`Producto eliminado con éxito: ${deletedProduct.title}`);
+        // Encontrar el producto por su ID
+        const productIndex = prods.findIndex(product => product.id === id);
+    
+        if (productIndex === -1) {
+          throw error(`Error: no se encontró ningún producto con id ${id}`);
+        }
+        // Eliminar el producto del arreglo de productos
+        const deletedProduct = prods.splice(productIndex, 1)[0];
+    
+        //Reescribe el archivo consultado con el producto actualizado convertido a String 
+        await fs.writeFile(this.path, JSON.stringify(prods))
+        return console.log(`Producto eliminado con éxito: ${deletedProduct.title}`);
+      }catch(error){
+        console.error(error);
+       }
     }
 }
   
 const productManager = new ProductManager('./info.txt');
-console.log(productManager.getProducts());
-  
-productManager.addProduct("Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", 8);
-console.log(productManager.getProducts());
+// console.log("inicio proceso");
+// console.log(productManager.getProducts());
 
-productManager.addProduct("Bajo mesada", "Mueble de cocina", 60000, "foto_2", "abc234", 8);
-console.log(productManager.getProducts());
 
-productManager.addProduct("Cama 2 plazas", "Mueble de dormitorio", 100000, "foto_3", "bcd123", 10);
-console.log(productManager.getProducts());
+await productManager.addProduct("Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", 8);
 
-productManager.addProduct("Cama 1 plazas", "Mueble de dormitorio", 80000, "foto_4", "bcd234", 20);
-console.log(productManager.getProducts());
+await productManager.addProduct("Bajo mesada", "Mueble de cocina", 60000, "foto_2", "abc234", 8);
 
-productManager.addProduct("Bibliteca 3 cuerpos", "Mueble de Living", 90000, "foto_5", "cde123", 5);
-console.log(productManager.getProducts());
+await productManager.addProduct("Cama 2 plazas", "Mueble de dormitorio", 100000, "foto_3", "bcd123", 10);
 
-productManager.addProduct("Bibliteca 2 cuerpos", "Mueble de Living", 70000, "foto_6", "cde234", 3);
-console.log(productManager.getProducts());
-  
-  
-// Agregar un nuevo producto con el mismo código (debe arrojar un error)
-productManager.addProduct("Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", 8);
+await productManager.addProduct("Cama 1 plazas", "Mueble de dormitorio", 80000, "foto_4", "bcd234", 20);
 
-// Agregar un nuevo producto con campo incompleto (debe arrojar un error)
-productManager.addProduct("Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", );
-  
-// Obtener un producto por su id (debe devolver el producto agregado)
-console.log(productManager.getProductById(4));
-  
-// Obtener un producto por un id inexistente (debe arrojar un error)
-productManager.getProductById(10);
+await productManager.addProduct("Bibliteca 3 cuerpos", "Mueble de Living", 90000, "foto_5", "cde123", 5);
 
-productManager.updateProduct(1, "Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", 9);
-console.log(productManager.getProducts());
+await productManager.addProduct("Bibliteca 2 cuerpos", "Mueble de Living", 70000, "foto_6", "cde234", 3);
+ 
+console.log("inicio proceso");
+// Agregar un nuevo producto con el mismo código
+await productManager.addProduct("Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", 8);
+console.log("inicio proceso");
+// Agregar un nuevo producto con campo incompleto
+await productManager.addProduct("Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", );
 
-// Actualizar un producto no existente
-productManager.updateProduct(10, "Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", 9);
-// Debe mostrar un error ya que el producto con ID 100 no existe
+//Obtener un producto por su id
+console.log("inicio proceso");
+await productManager.getProductById(2);
+console.log("fin proceso");
+
+// Obtener un producto por un id inexistente
+console.log("inicio proceso");
+await productManager.getProductById(10);
+console.log("fin proceso");
+
+console.log("inicio proceso");
+await productManager.updateProduct(1, "Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", 9);
+console.log("fin proceso");
+
+//Actualizar un producto no existente
+// console.log("inicio proceso");
+// await productManager.updateProduct(10, "Alacena", "Mueble de cocina", 50000, "foto_1", "abc123", 9);
+// console.log("fin proceso");
 
 // Eliminar un producto existente
-productManager.deleteProduct(6);
+console.log("inicio proceso");
+await productManager.deleteProduct(6);
+console.log("fin proceso");
 
 // Eliminar un producto no existente
-productManager.deleteProduct(10);
+console.log("inicio proceso");
+await productManager.deleteProduct(10);
+console.log("fin proceso");
+
